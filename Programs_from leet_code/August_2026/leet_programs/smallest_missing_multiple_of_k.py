8803d531a3d7a3d86d89067579283de5af48b413
Sourajit-balabantaray <sourajeetbalabantaray5@gmail.com>
2026-08-25 19:27:49 +0530

    smallest missing multiple of k using loop

---
+++ Programs_from leet_code/August_2026/leet_programs/smallest_missing_multiple_of_k.py
@@ -0,0 +1,8 @@
+class Solution:
+    def missingMultiple(self, nums: List[int], k: int) -> int:
+        
+        for i in range(1,len(nums)+2):
+            if i*k not in nums:
+                return i*k
+            
+        
