56ba1c03adff0a27ff47451d1198266a544887b0
Sourajit-balabantaray <sourajeetbalabantaray5@gmail.com>
2026-09-17 19:28:24 +0530

    Special array I

---
+++ Programs_from leet_code/September_2026/Special_array_I.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        def even(k):
+            if k%2==0:
+                return True
+            else:
+                return False
+        if len(nums)==1:
+            return True
+        if even(nums[0])==True:
+            for i in range(len(nums)):
+                if even(i)==True:
+                    if even(nums[i])==False:
+                        return False
+                else:
+                    if even(nums[i])==True:
+                        return False
+            return True
+        else:
+            for i in range(len(nums)):
+                if even(i)==False:
+                    if even(nums[i])==False:
+                        return False
+                else:
+                    if even(nums[i])==True:
+                        return False
+            return True
+
