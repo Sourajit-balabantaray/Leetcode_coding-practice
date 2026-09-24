3c59ced35b478f2d9834a05193e98c3ac55e8528
Sourajit-balabantaray <sourajeetbalabantaray5@gmail.com>
2026-09-24 16:56:23 +0530

    Smallest Index With Digit Sum Equal to Index

---
+++ Programs_from leet_code/September_2026/sum_digit_and_index.py
@@ -0,0 +1,17 @@
+class Solution:
+    def smallestIndex(self, nums: List[int]) -> int:
+        def dig(n):
+            s=0
+            while n>0:
+                d=n%10
+                s+=d
+                n=n//10
+            return s
+        l=[]
+        for i in range(len(nums)):
+            if dig(nums[i])==i:
+                l.append(i)
+        if not l:
+            return -1
+        return min(l)
+        
